f3fd3ffe430fc4545b7903ec79a73163feb7bc04
softgrowthinfotech01 <softgrowthinfotech@gmail.com>
2025-01-07 15:11:58 +0530

    database added

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php 
 	$servername="localhost";
 	$dbname="atmamaha_atma";
-	$username="atmamaha_atma";
-	$password="8o17zgEK25CC";
+	$username="root";
+	$password="";
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	date_default_timezone_set('Asia/Kolkata');
